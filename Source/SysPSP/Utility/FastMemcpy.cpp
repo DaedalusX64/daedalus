b92892effa3fd11f1d2bf3a0aae9169702cbfba9
salvy <salvy@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2012-12-11 18:45:14 +0000

    [~] Some fixes and improvements to my previous commit
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@861 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Source/SysPSP/Utility/FastMemcpy.cpp
+++ Source/SysPSP/Utility/FastMemcpy.cpp
@@ -95,43 +95,46 @@ void memcpy_vfpu_BE( void* dst, const void* src, size_t size )
 	else 	//At least src is 4byte and dst is 16byte aligned
     {
 		while (size>63)
-			{
-				asm(".set	push\n"					// save assembler option
-					".set	noreorder\n"			// suppress reordering
-					"ulv.q c000, 0(%1)\n"
-					"ulv.q c010, 16(%1)\n"
-					"ulv.q c020, 32(%1)\n"
-					"ulv.q c030, 48(%1)\n"
-					"sv.q c000, 0(%0)\n"
-					"sv.q c010, 16(%0)\n"
-					"sv.q c020, 32(%0)\n"
-					"sv.q c030, 48(%0)\n"
-					"addiu  %2, %2, -64\n"			//size -= 64;
-					"addiu	%1, %1, 64\n"			//dst8 += 64;
-					"addiu	%0, %0, 64\n"			//src8 += 64;
-					".set	pop\n"					// restore assembler option
-					:"+r"(dst8),"+r"(src8),"+r"(size)
-					:
-					:"memory"
-					);
-			}
+		{
+			asm(".set	push\n"					// save assembler option
+				".set	noreorder\n"			// suppress reordering
+				"ulv.q c000, 0(%1)\n"
+				"ulv.q c010, 16(%1)\n"
+				"ulv.q c020, 32(%1)\n"
+				"ulv.q c030, 48(%1)\n"
+				"sv.q c000, 0(%0)\n"
+				"sv.q c010, 16(%0)\n"
+				"sv.q c020, 32(%0)\n"
+				"sv.q c030, 48(%0)\n"
+				"addiu  %2, %2, -64\n"			//size -= 64;
+				"addiu	%1, %1, 64\n"			//dst8 += 64;
+				"addiu	%0, %0, 64\n"			//src8 += 64;
+				".set	pop\n"					// restore assembler option
+				:"+r"(dst8),"+r"(src8),"+r"(size)
+				:
+				:"memory"
+				);
+		}
 
-			while (size>15)
-			{
-				asm(".set	push\n"					// save assembler option
-					".set	noreorder\n"			// suppress reordering
-					"ulv.q c000, 0(%1)\n"
-					"sv.q c000, 0(%0)\n"
-					"addiu  %2, %2, -16\n"			//size -= 16;
-					"addiu	%1, %1, 16\n"			//dst8 += 16;
-					"addiu	%0, %0, 16\n"			//src8 += 16;
-					".set	pop\n"					// restore assembler option
-					:"+r"(dst8),"+r"(src8),"+r"(size)
-					:
-					:"memory"
-					);
-			}
+		while (size>15)
+		{
+			asm(".set	push\n"					// save assembler option
+				".set	noreorder\n"			// suppress reordering
+				"ulv.q c000, 0(%1)\n"
+				"sv.q c000, 0(%0)\n"
+				"addiu  %2, %2, -16\n"			//size -= 16;
+				"addiu	%1, %1, 16\n"			//dst8 += 16;
+				"addiu	%0, %0, 16\n"			//src8 += 16;
+				".set	pop\n"					// restore assembler option
+				:"+r"(dst8),"+r"(src8),"+r"(size)
+				:
+				:"memory"
+				);
+		}
     }
+
+	// fast out
+	if (size == 0) return;
 
 	dst32=(u32*)dst8;
 	src32=(u32*)src8;
@@ -503,9 +506,6 @@ void memcpy_cpu_LE( void* dst, const void* src, size_t size )
 	u32 *dst32;
 	u32 *src32;
 
-	// < 4 isn't worth trying any optimisations...
-	if( size<4 ) goto bytecopy;
-
 	// Align dst on 4 bytes or just resume if already done
 	while( ((u32)dst8&0x3)!=0 )
 	{
@@ -514,64 +514,64 @@ void memcpy_cpu_LE( void* dst, const void* src, size_t size )
 	}
 
 	// < 16 OR src not at least 32bit aligned at this point -> not worth trying any VFPU optimisations...
-	if( (((u32)src8&0x3) != (size<16)) ) goto normalcopy;
-
-	src32 = (u32*)src8;
-	dst32 = (u32*)dst8;
-
-	// Align dst to 16 bytes or just resume if already done
-	while( ((u32)dst32&0xF)!=0 )
+	if( (((u32)src8&0x3) == (size<16)) )
 	{
-		*dst32++ = *src32++;
-		size -= 4;
-	}
+		src32 = (u32*)src8;
+		dst32 = (u32*)dst8;
 
-	src8 = (u8*)src32;
-	dst8 = (u8*)dst32;
+		// Align dst to 16 bytes or just resume if already done
+		while( ((u32)dst32&0xF)!=0 )
+		{
+			*dst32++ = *src32++;
+			size -= 4;
+		}
+
+		src8 = (u8*)src32;
+		dst8 = (u8*)dst32;
 	
-	if( ((u32)src8&0xF)==0 )	//Both src and dst are 16byte aligned
-	{
-		while (size>63)
+		if( ((u32)src8&0xF)==0 )	//Both src and dst are 16byte aligned
 		{
-			asm(".set	push\n"					// save assembler option
-				".set	noreorder\n"			// suppress reordering
-				"lv.q c000, 0(%1)\n"
-				"lv.q c010, 16(%1)\n"
-				"lv.q c020, 32(%1)\n"
-				"lv.q c030, 48(%1)\n"
-				"sv.q c000, 0(%0)\n"
-				"sv.q c010, 16(%0)\n"
-				"sv.q c020, 32(%0)\n"
-				"sv.q c030, 48(%0)\n"
-				"addiu  %2, %2, -64\n"			//size -= 64;
-				"addiu	%1, %1, 64\n"			//dst8 += 64;
-				"addiu	%0, %0, 64\n"			//src8 += 64;
-				".set	pop\n"					// restore assembler option
-				:"+r"(dst8),"+r"(src8),"+r"(size)
-				:
-				:"memory"
-				);
-		}
+			while (size>63)
+			{
+				asm(".set	push\n"					// save assembler option
+					".set	noreorder\n"			// suppress reordering
+					"lv.q c000, 0(%1)\n"
+					"lv.q c010, 16(%1)\n"
+					"lv.q c020, 32(%1)\n"
+					"lv.q c030, 48(%1)\n"
+					"sv.q c000, 0(%0)\n"
+					"sv.q c010, 16(%0)\n"
+					"sv.q c020, 32(%0)\n"
+					"sv.q c030, 48(%0)\n"
+					"addiu  %2, %2, -64\n"			//size -= 64;
+					"addiu	%1, %1, 64\n"			//dst8 += 64;
+					"addiu	%0, %0, 64\n"			//src8 += 64;
+					".set	pop\n"					// restore assembler option
+					:"+r"(dst8),"+r"(src8),"+r"(size)
+					:
+					:"memory"
+					);
+			}
 
-		while (size>15)
-		{
-			asm(".set	push\n"					// save assembler option
-				".set	noreorder\n"			// suppress reordering
-				"lv.q c000, 0(%1)\n"
-				"sv.q c000, 0(%0)\n"
-				"addiu  %2, %2, -16\n"			//size -= 16;
-				"addiu	%1, %1, 16\n"			//dst8 += 16;
-				"addiu	%0, %0, 16\n"			//src8 += 16;
-				".set	pop\n"					// restore assembler option
-				:"+r"(dst8),"+r"(src8),"+r"(size)
-				:
-				:"memory"
-				);
+			while (size>15)
+			{
+				asm(".set	push\n"					// save assembler option
+					".set	noreorder\n"			// suppress reordering
+					"lv.q c000, 0(%1)\n"
+					"sv.q c000, 0(%0)\n"
+					"addiu  %2, %2, -16\n"			//size -= 16;
+					"addiu	%1, %1, 16\n"			//dst8 += 16;
+					"addiu	%0, %0, 16\n"			//src8 += 16;
+					".set	pop\n"					// restore assembler option
+					:"+r"(dst8),"+r"(src8),"+r"(size)
+					:
+					:"memory"
+					);
+			}
 		}
-	}
-	else 	//At least src is 4byte and dst is 16byte aligned
-    {
-		while (size>63)
+		else 	//At least src is 4byte and dst is 16byte aligned
+		{
+			while (size>63)
 			{
 				asm(".set	push\n"					// save assembler option
 					".set	noreorder\n"			// suppress reordering
@@ -608,24 +608,40 @@ void memcpy_cpu_LE( void* dst, const void* src, size_t size )
 					:"memory"
 					);
 			}
-    }
-
-normalcopy:
-	//Use CPU
-	//At least dst is aligned
-	dst32 = (u32*)dst8;
-	while(size>3)
-	{
-		register u32 tmp;
-		tmp = *(u8*)((u32)src8++ ^ 3);
-		tmp = (tmp << 8) | *(u8*)((u32)src8++ ^ 3);
-		tmp = (tmp << 8) | *(u8*)((u32)src8++ ^ 3);
-		*dst32++ = (tmp << 8) | *(u8*)((u32)src8++ ^ 3);
-		size -= 4;
+		}
 	}
-	dst8 = (u8*)dst32;
 
-bytecopy:
+	// fast out
+	if (size == 0) return;	
+
+	// Copy the remaning 32bit if any...
+	if( ((u32)src8&0x3)==0 )	//Src is 4byte aligned 
+	{	
+		src32 = (u32*)src8;
+		dst32 = (u32*)dst8;
+		while(size>3)
+		{
+			*dst32++ = *src32++;
+			size -= 4;
+		}
+		src8 = (u8*)src32;
+		dst8 = (u8*)dst32;
+	}
+	else	//At least dst is aligned
+	{
+		dst32 = (u32*)dst8;
+		while(size>3)
+		{
+			u32 a = *(u8*)((u32)src8++ ^ 3);
+			u32 b = *(u8*)((u32)src8++ ^ 3);
+			u32 c = *(u8*)((u32)src8++ ^ 3);
+			u32 d = *(u8*)((u32)src8++ ^ 3);
+			*dst32++ = (d << 24) | (c << 16) | (b << 8) | a;
+			size -= 4;
+		}
+		dst8 = (u8*)dst32;
+	}
+
 	// Copy the remaning bytes if any...
 	while (size--)
 	{
