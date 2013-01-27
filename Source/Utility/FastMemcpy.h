6d36f77ca059a1c69a31eafdc66b11e1f065b1aa
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-01-27 19:31:58 +0000

    [~] Define memcpy_swizzle first - it's a bit tidier
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@903 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Source/Utility/FastMemcpy.h
+++ Source/Utility/FastMemcpy.h
@@ -12,18 +12,6 @@ void memcpy_test( void * dst, const void * src, size_t size );
 void memcpy_byteswap( void* dst, const void* src, size_t size );	// Little endian, platform independent, ALWAYS swaps.
 
 
-#ifdef DAEDALUS_PSP
-
-void memcpy_vfpu( void* dst, const void* src, size_t size );
-void memcpy_vfpu_byteswap( void* dst, const void* src, size_t size );
-
-#define memcpy_swizzle 		memcpy_byteswap
-
-#define fast_memcpy 		memcpy_vfpu
-#define fast_memcpy_swizzle memcpy_vfpu_byteswap
-
-#else // DAEDALUS_PSP
-
 // memcpy_swizzle is just a regular memcpy on big-endian targets.
 #if (DAEDALUS_ENDIAN_MODE == DAEDALUS_ENDIAN_BIG)
 #define memcpy_swizzle 		memcpy
@@ -33,12 +21,23 @@ void memcpy_vfpu_byteswap( void* dst, const void* src, size_t size );
 #error No DAEDALUS_ENDIAN_MODE specified
 #endif
 
+
+// The PSP build defines fast versions of memcpy/memcpy_swizzle.
+#ifdef DAEDALUS_PSP
+
+void memcpy_vfpu( void* dst, const void* src, size_t size );
+void memcpy_vfpu_byteswap( void* dst, const void* src, size_t size );
+
+#define fast_memcpy 		memcpy_vfpu
+#define fast_memcpy_swizzle memcpy_vfpu_byteswap
+
+#else // DAEDALUS_PSP
+
 // Just use regular memcpy/memcpy_swizzle.
 #define fast_memcpy 		memcpy
 #define fast_memcpy_swizzle memcpy_swizzle
 
 #endif // DAEDALUS_PSP
-
 
 
 #endif // FASTMEMCPY_H_
