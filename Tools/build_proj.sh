13ec6913ea3678b53491c2d704fceac7ad8d6743
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-02-17 19:43:57 +0000

    [+] OSX build files
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1001 42e9bfbe-799a-4a2d-bad1-236e862a387a

---
+++ Tools/build_proj.sh
@@ -0,0 +1,8 @@
+export GYP_GENERATORS='ninja'
+export GYP_DEFINES="clang=1"
+export PATH=$PATH:~/clang3.1/bin
+
+export CC=~/clang3.1/bin/clang
+export CXX=~/clang3.1/bin/clang++
+
+./build/gyp/gyp --depth=0 --toplevel-dir . ./daedalus.gyp
