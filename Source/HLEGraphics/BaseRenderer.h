29155d7e0161b465b2b19cf62236c56357e5b574
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-03-03 12:06:01 +0000

    [~] Move DrawTexture functions from BaseRenderer to the platform-specific impls
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1116 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Source/HLEGraphics/BaseRenderer.h
+++ Source/HLEGraphics/BaseRenderer.h
@@ -224,9 +224,9 @@ public:
 
 	// Texture stuff
 	inline void			SetTextureScale(float fScaleX, float fScaleY)	{ mTnL.TextureScaleX = fScaleX; mTnL.TextureScaleY = fScaleY; }
-	void                Draw2DTextureBlit(float, float, float, float, float, float, float, float,CNativeTexture *texture);
-	void                Draw2DTexture(float, float, float, float, float, float, float, float);
-	void				Draw2DTextureR( f32 x0, f32 y0, f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3, f32 s, f32 t);
+	virtual void		Draw2DTextureBlit(f32 x, f32 y, f32 width, f32 height, f32 u0, f32 v0, f32 u1, f32 v1, CNativeTexture * texture) = 0;
+	virtual void		Draw2DTexture(f32 frameX, f32 frameY, f32 frameW, f32 frameH, f32 imageX, f32 imageY, f32 imageW, f32 imageH) = 0;
+	virtual void		Draw2DTextureR(f32 x0, f32 y0, f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3, f32 s, f32 t) = 0;
 
 
 	// Viewport stuff
