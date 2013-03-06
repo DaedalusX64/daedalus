6e472952c6bd8f39acd967aefd77c493adc8b83f
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-03-06 21:06:45 +0000

    [~] Bodgy input for OSX
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1188 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Source/SysOSX/Input/InputManagerOSX.cpp
+++ Source/SysOSX/Input/InputManagerOSX.cpp
@@ -20,6 +20,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "stdafx.h"
 #include "Input/InputManager.h"
 
+#include <GL/glfw.h>
+
 class IInputManager : public CInputManager
 {
 public:
@@ -61,6 +63,29 @@ bool IInputManager::GetState( OSContPad pPad[4] )
 		pPad[cont].stick_x = 0;
 		pPad[cont].stick_y = 0;
 	}
+
+	if (glfwGetKey( 'A' ))		pPad[0].button |= START_BUTTON;
+	if (glfwGetKey( 'S' ))		pPad[0].button |= A_BUTTON;
+	if (glfwGetKey( 'X' ))		pPad[0].button |= B_BUTTON;
+	if (glfwGetKey( 'Z' ))		pPad[0].button |= Z_TRIG;
+	if (glfwGetKey( 'Y' ))		pPad[0].button |= Z_TRIG;		// For German keyboards :)
+	if (glfwGetKey( 'C' ))		pPad[0].button |= L_TRIG;
+	if (glfwGetKey( 'V' ))		pPad[0].button |= R_TRIG;
+
+	if (glfwGetKey( 'T' ))		pPad[0].button |= U_JPAD;
+	if (glfwGetKey( 'G' ))		pPad[0].button |= D_JPAD;
+	if (glfwGetKey( 'F' ))		pPad[0].button |= L_JPAD;
+	if (glfwGetKey( 'H' ))		pPad[0].button |= R_JPAD;
+
+	if (glfwGetKey( 'I' ))		pPad[0].button |= U_CBUTTONS;
+	if (glfwGetKey( 'K' ))		pPad[0].button |= D_CBUTTONS;
+	if (glfwGetKey( 'J' ))		pPad[0].button |= L_CBUTTONS;
+	if (glfwGetKey( 'L' ))		pPad[0].button |= R_CBUTTONS;
+
+	if (glfwGetKey( GLFW_KEY_LEFT ))	pPad[0].stick_x = -80;
+	if (glfwGetKey( GLFW_KEY_RIGHT ))	pPad[0].stick_x = +80;
+	if (glfwGetKey( GLFW_KEY_UP ))		pPad[0].stick_y = +80;
+	if (glfwGetKey( GLFW_KEY_DOWN ))	pPad[0].stick_y = -80;
 
 	return true;
 }
