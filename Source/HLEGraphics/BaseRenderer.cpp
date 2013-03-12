062e7b40a07051e46f4de5986e9fd6d35478d7ef
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-03-12 20:50:55 +0000

    [~] Improve the '0 width/height' assert slightly
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1259 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Source/HLEGraphics/BaseRenderer.cpp
+++ Source/HLEGraphics/BaseRenderer.cpp
@@ -1731,7 +1731,7 @@ void BaseRenderer::UpdateTileSnapshot( u32 index, u32 tile_idx )
 	// Check for 0 width/height textures
 	if( ti.GetWidth() == 0 || ti.GetHeight() == 0 )
 	{
-		DAEDALUS_DL_ERROR( "Loading texture with 0 width/height" );
+		DAEDALUS_DL_ERROR( "Loading texture with 0 width/height in slot %d", index );
 	}
 	else
 	{
