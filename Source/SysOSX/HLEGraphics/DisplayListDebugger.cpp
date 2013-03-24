57ef91be5cce73b3b435b8f5789419c60583aee9
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-03-24 22:09:20 +0000

    [+] Screenshot code for the web DLDebugger
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1422 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Source/SysOSX/HLEGraphics/DisplayListDebugger.cpp
+++ Source/SysOSX/HLEGraphics/DisplayListDebugger.cpp
@@ -4,9 +4,25 @@
 #include "SysOSX/Debug/WebDebug.h"
 #include "SysOSX/Debug/WebDebugTemplate.h"
 
+#include "Graphics/GraphicsContext.h"
+#include "Graphics/PngUtil.h"
+
+#include "Utility/Mutex.h"
+
+#include <GL/glfw.h>
+
 #ifdef DAEDALUS_DEBUG_DISPLAYLIST
 
 static bool gDebugging = false;
+
+// These coordinate requests between the web threads and the main thread (only the main thread can call OpenGL functions)
+static GLFWcond gScreenshotCond   = NULL;
+static GLFWmutex gScreenshotMutex = NULL;
+
+// This mutex is to ensure that only one connection can request a screenshot at a time.
+static Mutex gSerialiseScreenshots("Screenshots");
+
+static WebDebugConnection * gScreenshotConnection = NULL;
 
 bool DLDebugger_IsDebugging()
 {
@@ -25,7 +41,48 @@ bool DLDebugger_Process()
 		printf("Debugging\n");
 		gDebugging = false;
 	}
+
+	// Check if a web request is waiting for a screenshot.
+	glfwLockMutex(gScreenshotMutex);
+	if (WebDebugConnection * connection = gScreenshotConnection)
+	{
+		u32 width;
+		u32 height;
+		CGraphicsContext::Get()->GetScreenSize(&width, &height);
+
+		// Make the BYTE array, factor of 3 because it's RBG.
+		void * pixels = malloc( 4 * width * height );
+
+		glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+		// NB, pass a negative pitch, to render the screenshot the right way ip.
+		s32 pitch = -(width * 4);
+
+		PngSaveImage(connection, pixels, NULL, TexFmt_8888, pitch, width, height, false);
+
+		free(pixels);
+
+		connection->EndResponse();
+		gScreenshotConnection = NULL;
+	}
+	glfwUnlockMutex(gScreenshotMutex);
+	glfwSignalCond(gScreenshotCond);
+
 	return false;
+}
+
+static void TakeScreenshot(WebDebugConnection * connection)
+{
+	connection->BeginResponse(200, -1, "image/png" );
+
+	// Only one request for a screenshot can be serviced at a time.
+	MutexLock lock(&gSerialiseScreenshots);
+
+	// Request a screenshot from the main thread.
+	glfwLockMutex(gScreenshotMutex);
+	gScreenshotConnection = connection;
+	glfwWaitCond(gScreenshotCond, gScreenshotMutex, GLFW_INFINITY);
+	glfwUnlockMutex(gScreenshotMutex);
 }
 
 static void DLDebugHandler(void * arg, WebDebugConnection * connection)
@@ -42,8 +99,15 @@ static void DLDebugHandler(void * arg, WebDebugConnection * connection)
 					DLDebugger_RequestDebug();
 				}
 			}
+			else if (params[i].Key == "setcmd")
+			{
+				//int cmd = atoi(params[i].Value);
+				TakeScreenshot(connection);
+				return;
+			}
 		}
 
+		// Fallthrough for handlers that just return 'ok'.
 		connection->BeginResponse(200, -1, "text/plain" );
 		connection->WriteString("ok\n");
 		connection->EndResponse();
@@ -75,6 +139,8 @@ bool DLDebugger_RegisterWebDebug()
 #ifdef DAEDALUS_DEBUG_DISPLAYLIST
 	WebDebug_Register( "/dldebugger", &DLDebugHandler, NULL );
 #endif
+	gScreenshotCond = glfwCreateCond();
+	gScreenshotMutex = glfwCreateMutex();
 	return true;
 }
 
