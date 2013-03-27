bb2ab004abb4eb0ac22e9792842cd7936b0d290c
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-03-27 22:07:50 +0000

    [~] Use the default version of clang
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1456 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Tools/build_proj.sh
+++ Tools/build_proj.sh
@@ -1,8 +1,7 @@
 export GYP_GENERATORS='ninja'
 export GYP_DEFINES="clang=1"
-export PATH=$PATH:~/clang3.1/bin
 
-export CC=~/clang3.1/bin/clang
-export CXX=~/clang3.1/bin/clang++
+export CC=clang
+export CXX=clang++
 
 ./Tools/build/gyp/gyp --depth=0 --toplevel-dir . ./daedalus.gyp
