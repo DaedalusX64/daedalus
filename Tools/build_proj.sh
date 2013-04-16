687cc87c95c504326accff564c565c21ee156c88
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-04-16 12:04:14 +0000

    [~] Move daedalus.gyp to the Source dir
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1589 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Tools/build_proj.sh
+++ Tools/build_proj.sh
@@ -4,4 +4,4 @@ export GYP_DEFINES="clang=1"
 export CC=clang
 export CXX=clang++
 
-./Tools/build/gyp/gyp --depth=0 --toplevel-dir . ./Projects/OSX/daedalus.gyp
+./Tools/build/gyp/gyp --depth=0 --toplevel-dir . ./Source/daedalus.gyp
