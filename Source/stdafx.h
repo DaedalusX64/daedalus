022c740bbb7a20cb5c0f0179ca7344a784515391
strmnnrmn <strmnnrmn@42e9bfbe-799a-4a2d-bad1-236e862a387a>
2013-05-18 16:06:20 +0000

    [~] stdafx cleanup: DaedalusAssert.h is already included by DaedalusTypes.h
    git-svn-id: https://subversion.assembla.com/svn/Daedalusx64/trunk@1697 42e9bfbe-799a-4a2d-bad1-236e862a387a

--- Source/stdafx.h
+++ Source/stdafx.h
@@ -40,7 +40,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "DaedalusW32.h"
 #endif
 
-#include "Debug/DaedalusAssert.h"
 #include "Utility/DaedalusTypes.h"
 
 #endif // PRECOMPILED_H_
