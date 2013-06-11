d6ae6d27ee2e4043076f948e2974ee3a4752d10a
salvy <salvatorecolon@gmail.com>
2013-06-11 12:09:20 -0700

    [!] Make sure to use the bswap op for W32 (I'm surprised MSVC didn't optimized this out..)

--- Source/Utility/Endian.h
+++ Source/Utility/Endian.h
@@ -25,20 +25,36 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define UTILITY_ENDIAN_H_
 
 #if (DAEDALUS_ENDIAN_MODE == DAEDALUS_ENDIAN_BIG)
-#define BSWAP32(x) x
-#define U8_TWIDDLE 0x0
-#define U16_TWIDDLE 0x0
+
+	#define U8_TWIDDLE 0x0
+	#define U16_TWIDDLE 0x0
+	#define BSWAP32(x) x
+
 #elif (DAEDALUS_ENDIAN_MODE == DAEDALUS_ENDIAN_LITTLE)
-#ifdef DAEDALUS_PSP
-#define BSWAP32(x) __builtin_bswap32(x)
+	#define U8_TWIDDLE 0x3
+	#define U16_TWIDDLE 0x2
+	#define U16H_TWIDDLE 0x1
+
+	#if defined(DAEDALUS_PSP)
+
+		#define BSWAP32(x) __builtin_bswap32(x)
+
+	#elif defined(DAEDALUS_W32)
+
+		#if defined( _MSC_VER )
+			#define BSWAP32(x) _byteswap_ulong(x)
+		#elif defined( __GNUC__ )
+			#define BSWAP32(x) __builtin_bswap32(x)
+		#else
+			#error "Unhandled compiler type"
+		#endif	
+
+	#else
+		#define BSWAP32(x) ((x >> 24) | ((x >> 8) & 0xFF00) | ((x & 0xFF00) << 8) | (x << 24))
+	#endif
+
 #else
-#define BSWAP32(x) ((x >> 24) | ((x >> 8) & 0xFF00) | ((x & 0xFF00) << 8) | (x << 24))
-#endif
-#define U8_TWIDDLE 0x3
-#define U16_TWIDDLE 0x2
-#define U16H_TWIDDLE 0x1
-#else
-#error No DAEDALUS_ENDIAN_MODE specified
+	#error No DAEDALUS_ENDIAN_MODE specified
 #endif
 
 
