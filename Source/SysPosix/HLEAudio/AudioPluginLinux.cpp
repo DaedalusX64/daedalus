df3b227cecff9f2ab297e056f9e0d5b04d9f727f
Ben Slater <catapultit@me.com>
2019-02-19 18:18:36 +1100

    Added basic audio framework to Linux(SDL) Audio Plugin

--- Source/SysPosix/HLEAudio/AudioPluginLinux.cpp
+++ Source/SysPosix/HLEAudio/AudioPluginLinux.cpp
@@ -8,3 +8,100 @@ CAudioPlugin * CreateAudioPlugin()
 {
 	return NULL;
 }
+
+class AudioPluginSDL : public CAudioPlugin
+{
+public:
+	AudioPluginSDL();
+	virtual ~AudioPluginSDL();
+
+	virtual bool			StartEmulation();
+	virtual void			StopEmulation();
+
+	virtual void			DacrateChanged(int system_type);
+	virtual void			LenChanged();
+	virtual u32				ReadLength()			{ return 0; }
+	virtual EProcessResult	ProcessAList();
+
+
+	void					StopAudio();						// Stops the Audio PlayBack (as if paused)
+	void					StartAudio();						// Starts the Audio PlayBack (as if unpaused)
+
+
+private:
+	CAudioBuffer			mAudioBuffer;
+	u32						mFrequency;
+	ThreadHandle 			mAudioThread;
+	volatile bool			mKeepRunning;	// Should the audio thread keep running?
+
+	volatile u32 			mBufferLenMs;
+};
+
+AudioPluginSDL::~AudioPluginSDL()
+{
+	StopAudio();
+}
+
+AudioPluginSDL::StartEmulation()
+{
+	return true;
+}
+
+AudioPluginSDL::StopEmulation()
+{
+	Audio_Reset();
+	StopAudio();
+}
+
+void AudioPluginSDL::DacrateChanged(int system_type)
+{
+	u32 clock = (system_type ==ST_NTSC) ? VI_NTSC_CLOCK : VI_PAL_CLOCK;
+	u32 dacrate = Memory_AI_GetRegister(AI_DACRATE_REG);
+	u32 frequency = clock / (dacrate + 1);
+
+	DBGConsole_Msg(0, "Audio Frequency: %d", frequency);
+	mFrequency = frequency;
+
+}
+
+void AudioPluginSDL::LenChanged()
+{
+	if(gAudioPluginEnabled > APM_DISABLED)
+	{
+		u32 address = Memory_AI_GetRegister(AI_DRAM_ADDR_REG) & 0xFFFFFF;
+		u32 length = Memory_AI_GetRegister(AI_LEN_REG);
+
+		AddBuffer( g_pu8RamBase + address, length)
+	}
+	else
+	{
+		stopAudio();
+	}
+
+	EProcessResult AudioPluginSDL::ProcessAList()
+	{
+		Memory_SP_SetRegisterBits(SP_STATUS_REG, SP_STATUS_HALT);
+
+		EProcessResult result = PR_NOT_STARTED;
+
+				switch (gAudioPluginEnabled)
+				{
+					case APM_DISABLED:
+						result = PR_COMPLETED;
+						break;
+						case APM_ENABLED_ASYNC:
+							DAEDALUS_ERROR("Async audio is unimplemented");
+							Audio_Ucode();
+							result = PR_COMPLETED;
+							break;
+						case APM_ENABLED_SYNC:
+							Audio_Ucode();
+							result = PR_COMPLETED;
+							break;
+				}
+
+				return result;
+		}
+	}
+	}
+}
