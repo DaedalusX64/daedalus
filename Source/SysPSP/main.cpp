6ad4998ebc9622dba88f93f3a045362bd9b39109
z2442 <z2442@aol.com>
2019-02-19 21:41:00 -0500

    clean up commented vita code. wont be needed anymore

--- Source/SysPSP/main.cpp
+++ Source/SysPSP/main.cpp
@@ -1,21 +1,21 @@
 /*
  Copyright (C) 2006,2007 StrmnNrmn
  Copyright (C) 2018,2019 DaedalusX64 Team (z2442, Wally4000)
- 
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
  of the License, or (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- 
+
  */
 
 #include "stdafx.h"
@@ -64,13 +64,6 @@
 #include "Utility/Thread.h"
 #include "Utility/Timer.h"
 
-/* unfinished, Vita detection is currently pure psuedocode
- #ifdef VITA
- #include <psp2/power.h>
- #include <psp2/sysmodule.h>
- #include <psp2/appmgr.h>
- */
-
 
 /* Define to enable Exit Callback */
 // Do not enable this, callbacks don't get along with our exit dialog :p
@@ -87,11 +80,11 @@ extern "C"
 {
     /* Disable FPU exceptions */
     void _DisableFPUExceptions();
-    
+
     /* Video Manager functions */
     int pspDveMgrCheckVideoOut();
     int pspDveMgrSetVideoOut(int, int, int, int, int, int, int);
-    
+
 #ifdef DAEDALUS_PSP_GPROF
     /* Profile with psp-gprof */
     void gprof_cleanup();
@@ -128,7 +121,7 @@ static int ExitCallback( int arg1, int arg2, void * common )
 #ifdef DAEDALUS_PSP_GPROF
     gprof_cleanup();
 #endif
-    
+
     sceKernelExitGame();
     return 0;
 }
@@ -139,12 +132,12 @@ static int ExitCallback( int arg1, int arg2, void * common )
 static int CallbackThread( SceSize args, void * argp )
 {
     int cbid;
-    
+
     cbid = sceKernelCreateCallback( "Exit Callback", ExitCallback, NULL );
     sceKernelRegisterExitCallback( cbid );
-    
+
     sceKernelSleepThreadCB();
-    
+
     return 0;
 }
 
@@ -154,7 +147,7 @@ static int CallbackThread( SceSize args, void * argp )
 static int SetupCallbacks()
 {
     int thid = 0;
-    
+
     thid = sceKernelCreateThread( "CallbackThread", CallbackThread, 0x11, 0xFA0, PSP_THREAD_ATTR_USER, 0 );
     if(thid >= 0)
     {
@@ -170,15 +163,15 @@ static int SetupCallbacks()
 static int PanicThread( SceSize args, void * argp )
 {
     const u32 MASK = PSP_CTRL_LTRIGGER | PSP_CTRL_RTRIGGER | PSP_CTRL_START;
-    
+
     u32 count = 0;
-    
+
     //Loop 4 ever
     while(1)
     {
         SceCtrlData pad;
         sceCtrlPeekBufferPositive(&pad, 1);
-        
+
         if( (pad.Buttons & MASK) == MASK )
         {
             if(++count > 5)         //If button press for more that 2sec we return to main menu
@@ -190,11 +183,11 @@ static int PanicThread( SceSize args, void * argp )
             }
         }
         else count = 0;
-        
+
         //Idle here, only check button 3 times/sec not to hog CPU time from EMU
         ThreadSleepMs(300);
     }
-    
+
     return 0;
 }
 
@@ -204,12 +197,12 @@ static int PanicThread( SceSize args, void * argp )
 static int SetupPanic()
 {
     int thidf = sceKernelCreateThread( "PanicThread", PanicThread, 0x18, 0xFA0, PSP_THREAD_ATTR_USER, 0 );
-    
+
     if(thidf >= 0)
     {
         sceKernelStartThread(thidf, 0, 0);
     }
-    
+
     return 0;
 }
 
@@ -220,66 +213,58 @@ extern bool InitialiseJobManager();
 //*************************************************************************************
 static bool    Initialize()
 {
-    
+
     strcpy(gDaedalusExePath, DAEDALUS_PSP_PATH( "" ));
-    
-    /*
-     #ifdef VITA
-     scePowerSetArmClockFrequency(444);
-     scePowerSetBusClockFrequency(222);
-     scePowerSetGpuClockFrequency(222);
-     scePowerSetGpuXbarClockFrequency(166);
-     */
-    
+
     printf( "Cpu was: %dMHz, Bus: %dMHz\n", scePowerGetCpuClockFrequency(), scePowerGetBusClockFrequency() );
     if (scePowerSetClockFrequency(333, 333, 166) != 0)
     {
         printf( "Could not set CPU to 333MHz\n" );
     }
     printf( "Cpu now: %dMHz, Bus: %dMHz\n", scePowerGetCpuClockFrequency(), scePowerGetBusClockFrequency() );
-    
+
     // Set up our Kernel Home button
     //ToDo: This doesn't work properly for Vita, there's no longer a "home" button available
     InitHomeButton();
-    
+
     // If (o) is pressed during boot the Emulator will use 32bit
     // else use default 16bit color mode
     SceCtrlData pad;
     sceCtrlPeekBufferPositive(&pad, 1);
     if( pad.Buttons & PSP_CTRL_CIRCLE ) g32bitColorMode = true;
     else g32bitColorMode = false;
-    
+
     //File not found Activate the psp Media Engine.
     bool bMeStarted = InitialiseJobManager();
-    
-    
+
+
     // Disable for profiling
     //    srand(time(0));
-    
+
     //Set the debug output to default
     if( g32bitColorMode ) pspDebugScreenInit();
     else pspDebugScreenInitEx( NULL , GU_PSM_5650, 1); //Sets debug output to 16bit mode
-    
+
     // This Breaks gdb, better disable it in debug build
     //
 #ifndef FR
     initExceptionHandler();
 #endif
-    
+
     _DisableFPUExceptions();
-    
+
     //Init Panic button thread
     SetupPanic();
-    
+
     // Init volatile memory
     VolatileMemInit();
-    
+
 #ifdef DAEDALUS_CALLBACKS
     //Set up callback for our thread
     SetupCallbacks();
 #endif
-    
-    
+
+
     //Set up the DveMgr (TV Display) and Detect PSP Slim /3K/ Go
     if ( PSPDetect(0) )
     {
@@ -290,7 +275,7 @@ static bool    Initialize()
         // Can't use extra memory if ME isn't available
         if( bMeStarted )
             PSP_IS_SLIM = true;
-        
+
         HAVE_DVE = CModule::Load("dvemgr.prx");
         if (HAVE_DVE >= 0)
             PSP_TV_CABLE = pspDveMgrCheckVideoOut();
@@ -299,18 +284,18 @@ static bool    Initialize()
         else if( PSP_TV_CABLE == 0 )
             CModule::Unload( HAVE_DVE );    // Stop and unload dvemgr.prx since if no video cable is connected
     }
-    
+
     HAVE_DVE = (HAVE_DVE < 0) ? 0 : 1; // 0 == no dvemgr, 1 == dvemgr
-    
+
     sceCtrlSetSamplingCycle(0);
     sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
-    
+
     // Init the savegame directory
     strcpy( g_DaedalusConfig.mSaveDir, DAEDALUS_PSP_PATH( "SaveGames/" ) );
-    
+
     if (!System_Init())
         return false;
-    
+
     return true;
 }
 
@@ -335,40 +320,40 @@ static void    DumpDynarecStats( float elapsed_time )
     extern u32 gTotalRegistersUncached;
     extern u32 gFragmentLookupSuccess;
     extern u32 gFragmentLookupFailure;
-    
+
     u32        dynarec_ratio( 0 );
-    
+
     if(gTotalInstructionsExecuted + gTotalInstructionsEmulated > 0)
     {
         float fRatio = float(gTotalInstructionsExecuted * 100.0f / float(gTotalInstructionsEmulated+gTotalInstructionsExecuted));
-        
+
         dynarec_ratio = u32( fRatio );
-        
+
         //gTotalInstructionsExecuted = 0;
         //gTotalInstructionsEmulated = 0;
     }
-    
+
     u32        cached_regs_ratio( 0 );
     if(gTotalRegistersCached + gTotalRegistersUncached > 0)
     {
         float fRatio = float(gTotalRegistersCached * 100.0f / float(gTotalRegistersCached+gTotalRegistersUncached));
-        
+
         cached_regs_ratio = u32( fRatio );
     }
-    
+
     const char * const TERMINAL_SAVE_CURSOR            = "\033[s";
     const char * const TERMINAL_RESTORE_CURSOR        = "\033[u";
     //    const char * const TERMINAL_TOP_LEFT            = "\033[2A\033[2K";
     const char * const TERMINAL_TOP_LEFT            = "\033[H\033[2K";
-    
+
     printf( TERMINAL_SAVE_CURSOR );
     printf( TERMINAL_TOP_LEFT );
-    
+
     printf( "Fe: %dms, DynaRec %d%%, Regs cached %d%%, Lookup success %d/%d", u32(elapsed_time * 1000.0f), dynarec_ratio, cached_regs_ratio, gFragmentLookupSuccess, gFragmentLookupFailure );
-    
+
     printf( TERMINAL_RESTORE_CURSOR );
     fflush( stdout );
-    
+
     gFragmentLookupSuccess = 0;
     gFragmentLookupFailure = 0;
 }
@@ -391,9 +376,9 @@ void HandleEndOfFrame()
     if(DLDebugger_IsDebugging())
         return;
 #endif
-    
+
     DPF( DEBUG_FRAME, "********************************************" );
-    
+
     bool        activate_pause_menu( false );
     //
     //    Figure out how long the last frame took
@@ -406,9 +391,9 @@ void HandleEndOfFrame()
     //
     static u32 oldButtons = 0;
     SceCtrlData pad;
-    
+
     sceCtrlPeekBufferPositive(&pad, 1);
-    
+
     // If kernelbuttons.prx couldn't be loaded, allow select button to be used instead
     //
     if(oldButtons != pad.Buttons)
@@ -417,7 +402,7 @@ void HandleEndOfFrame()
         {
             CheatCodes_Activate( GS_BUTTON );
         }
-        
+
         if(pad.Buttons & PSP_CTRL_HOME)
         {
             while(!activate_pause_menu)
@@ -431,7 +416,7 @@ void HandleEndOfFrame()
         }
     }
     oldButtons = pad.Buttons;
-    
+
     if(activate_pause_menu)
     {
         // See how much texture memory we're using
@@ -439,33 +424,33 @@ void HandleEndOfFrame()
         //#ifdef DAEDALUS_DEBUG_MEMORY
         //CVideoMemoryManager::Get()->DisplayDebugInfo();
         //#endif
-        
+
         // No longer needed since we save normally now, and not jsut when entering the pause menu ;)
         //Save_Flush(true);
-        
+
         // switch back to the LCD display
         CGraphicsContext::Get()->SwitchToLcdDisplay();
-        
+
         // Call this initially, to tidy up any state set by the emulator
         CGraphicsContext::Get()->ClearAllSurfaces();
-        
+
         CDrawText::Initialise();
-        
+
         CUIContext *    p_context( CUIContext::Create() );
-        
+
         if(p_context != NULL)
         {
             // Already set in ClearBackground() @ UIContext.h
             //p_context->SetBackgroundColour( c32( 94, 188, 94 ) );        // Nice green :)
-            
+
             CPauseScreen *    pause( CPauseScreen::Create( p_context ) );
             pause->Run();
             delete pause;
             delete p_context;
         }
-        
+
         CDrawText::Destroy();
-        
+
         //
         // Commit the preferences database before starting to run
         //
@@ -477,7 +462,7 @@ void HandleEndOfFrame()
 #ifdef DAEDALUS_PROFILE_EXECUTION
     gTimer.Reset();
 #endif
-    
+
 }
 
 //*************************************************************************************
@@ -487,11 +472,11 @@ static void DisplayRomsAndChoose(bool show_splash)
 {
     // switch back to the LCD display
     CGraphicsContext::Get()->SwitchToLcdDisplay();
-    
+
     CDrawText::Initialise();
-    
+
     CUIContext *    p_context( CUIContext::Create() );
-    
+
     if(p_context != NULL)
     {
         // Already set in ClearBackground() @ UIContext.h
@@ -499,23 +484,23 @@ static void DisplayRomsAndChoose(bool show_splash)
         //const c32        BACKGROUND_COLOUR = c32( 92, 162, 219 );        // blue
         //const c32        BACKGROUND_COLOUR = c32( 1, 1, 127 );            // dark blue
         //const c32        BACKGROUND_COLOUR = c32( 1, 127, 1 );            // dark green
-        
+
         //p_context->SetBackgroundColour( BACKGROUND_COLOUR );
-        
+
         if( show_splash )
         {
             CSplashScreen *        p_splash( CSplashScreen::Create( p_context ) );
             p_splash->Run();
             delete p_splash;
         }
-        
+
         CMainMenuScreen *    p_main_menu( CMainMenuScreen::Create( p_context ) );
         p_main_menu->Run();
         delete p_main_menu;
     }
-    
+
     delete p_context;
-    
+
     CDrawText::Destroy();
 }
 #include "Utility/Translate.h"
@@ -554,20 +539,20 @@ extern "C"
             {
                 DisplayRomsAndChoose( show_splash );
                 show_splash = false;
-                
+
                 //
                 // Commit the preferences and roms databases before starting to run
                 //
                 CRomDB::Get()->Commit();
                 CPreferences::Get()->Commit();
-                
+
                 CPU_Run();
                 System_Close();
             }
-            
+
             Finalise();
         }
-        
+
         sceKernelExitGame();
         return 0;
     }
