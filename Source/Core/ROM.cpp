c8b7a08d29982bba6c95f6ad1a474b892940457d
joey <joey@joeys-iMac.fios-router.home>
2020-04-29 18:50:50 -0400

    Regression

--- Source/Core/ROM.cpp
+++ Source/Core/ROM.cpp
@@ -439,9 +439,6 @@ void SpecificGameHacks( const ROMHeader & id )
 	case 0x5543:	//Cruisn' USA
 		g_ROM.GameHacks = BODY_HARVEST;
 		break;
-	    case 0x424f: //OB 64
-        g_ROM.T1_HACK = true;
-        break;
 	default:
 		break;
 	}
