bb7d13ca06e591ce2be1a0696b308461259e6bbc
Wally4000 <catapultit@me.com>
2022-06-13 20:59:43 +1000

    Change CMake Script to allow installing

--- build_daedalus.sh
+++ build_daedalus.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
 
-## This file is the standard way of building Daedalus, you may build manually using cmake but your milage may vary on certain platforms.
-
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
+
+## This file is the standard way of building Daedalus, you may build manually using cmake but your milage may vary on certain platforms.
 
 function pre_prep() {
 # Start Fresh, remove any existing builds
-rm -r $PWD/build $PWD/DaedalusX64/EBOOT.PBP $PWD/DaedalusX64/daedalus >/dev/null 2>&1
-mkdir -p $PWD/DaedalusX64
+ rm -r $PWD/build $PWD/DaedalusX64/EBOOT.PBP $PWD/DaedalusX64/daedalus >/dev/null 2>&1
 }
 
-function finalPrep() {
-    if [[ -f "$PWD/EBOOT.PBP" ]]; then
-        mv "$PWD/EBOOT.PBP" ../DaedalusX64
-    else
-        cp -r "$PWD/daedalus" ../DaedalusX64
-        cp ../Source/SysGL/HLEGraphics/n64.psh ../DaedalusX64 
-    fi
-         cp -r ../Data/* ../DaedalusX64/
-}
 
-function psp_plugins() {
-  make --quiet -j $PROC_NR -C "$PWD/../Source/SysPSP/PRX/DveMgr" || { exit 1; }
-  make --quiet -j $PROC_NR -C "$PWD/../Source/SysPSP/PRX/ExceptionHandler" || { exit 1; }
-  make --quiet -j $PROC_NR -C "$PWD/../Source/SysPSP/PRX/KernelButtons" || { exit 1; }
-  make --quiet -j $PROC_NR -C "$PWD/../Source/SysPSP/PRX/MediaEngine" || { exit 1; }
-}
+ function psp_plugins() {
+   make --quiet -j $PROC_NR -C "$PWD/Source/SysPSP/PRX/DveMgr" || { exit 1; }
+   make --quiet -j $PROC_NR -C "$PWD/Source/SysPSP/PRX/ExceptionHandler" || { exit 1; }
+   make --quiet -j $PROC_NR -C "$PWD/Source/SysPSP/PRX/KernelButtons" || { exit 1; }
+   make --quiet -j $PROC_NR -C "$PWD/Source/SysPSP/PRX/MediaEngine" || { exit 1; }
+ }
 
 function build() {
 
 ## Compile and install.
 make --quiet -j $PROC_NR  || { exit 1; }
-finalPrep
 }
 
 if [[ $1 = "DEBUG" ]] || [[ $2 = "DEBUG" ]]; then
@@ -41,14 +30,14 @@ fi
 ## Main Loop
 
  pre_prep
- mkdir -p build && cd build
 
 case "$1" in
     PSP)
-    mkdir -p "$PWD/DaedalusX64/Plugins"
     psp_plugins
-    cmake -DCMAKE_TOOLCHAIN_FILE=$PSPDEV/psp/share/pspdev.cmake $CMAKEDEFINES ../Source
-    build
+    cmake -DCMAKE_TOOLCHAIN_FILE=$PSPDEV/psp/share/pspdev.cmake $CMAKEDEFINES -S . -B build 
+    cmake --build build -j${PROC_NR}
+    cmake --install build --prefix .
+    # cmake --install build
     ;;
     **) ## Not Cross Compiling (macOS, Windows (Not yet), Linux)
     cmake ../Source $CMAKEDEFINES
