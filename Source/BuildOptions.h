3ffdbf84f748ea314dc2c54460217887ce1fdd49
Wally4000 <catapultit@me.com>
2022-06-30 22:19:20 +1000

    Replace u32 with unsigned int for PSP

--- Source/BuildOptions.h
+++ Source/BuildOptions.h
@@ -2,6 +2,7 @@
 #define BUILDOPTIONS_H_
 
 #include "Base/Types.h"
+#include <cstdint>
 // All Custom Macro Functions live here until CMake supports them.
 
 // Defines same for all 
@@ -20,7 +21,7 @@
 #ifdef DAEDALUS_PSP
     template <typename T>
     T make_uncached_ptr(T ptr) {
-        return reinterpret_cast<T>(reinterpret_cast<u32>(ptr) | 0x40000000);
+        return reinterpret_cast<T>(reinterpret_cast<unsigned int>(ptr) | 0x40000000);
     }
 #else
 #define make_uncached_ptr(x)	(x)
