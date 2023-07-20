d90f9c4e915842a394424af72b12ee618c7d7480
Wally4000 <catapultit@me.com>
2023-07-20 16:02:09 +1000

    Tidy up SDL Audio Code

--- Source/SysPosix/HLEAudio/AudioPluginSDL.cpp
+++ Source/SysPosix/HLEAudio/AudioPluginSDL.cpp
@@ -1,48 +1,24 @@
-#include "BuildOptions.h"
-#include "Base/Types.h"
-#include "HLEAudio/AudioPlugin.h"
-#include "Config/ConfigOptions.h"
-#include <stdio.h>
 #include <SDL2/SDL.h>
+
+#include "BuildOptions.h"
+
+#include "Base/Types.h"
 #include "Config/ConfigOptions.h"
+#include "Core/FramerateLimiter.h"
 #include "Core/Memory.h"
 #include "Debug/DBGConsole.h"
-#include "HLEAudio/AudioBuffer.h"
+#include "HLEAudio/AudioPlugin.h"
 #include "HLEAudio/HLEAudioInternal.h"
-#include "Core/FramerateLimiter.h"
-#include "System/Thread.h"
 #include "System/Timing.h"
 
 EAudioPluginMode gAudioPluginEnabled = APM_ENABLED_SYNC;
 
-#define DEBUG_AUDIO  0
-
-#if DEBUG_AUDIO
-#define DPF_AUDIO(...)	do { printf(__VA_ARGS__); } while(0)
-#else
-#define DPF_AUDIO(...)	do { (void)sizeof(__VA_ARGS__); } while(0)
-#endif
-
-static const u32 kOutputFrequency = 44100;
-static const u32 kAudioBufferSize = 1024 * 1024;	// Circular buffer length. Converts N64 samples out our output rate.
-static const u32 kNumChannels = 2;
-
-// How much input we try to keep buffered in the synchronisation code.
-// Setting this too low and we run the risk of skipping.
-// Setting this too high and we run the risk of being very laggy.
-static const u32 kMaxBufferLengthMs = 30;
-
-// AudioQueue buffer object count and length.
-// Increasing either of these numbers increases the amount of buffered
-// audio which can help reduce crackling (empty buffers) at the cost of lag.
-static const u32 kNumBuffers = 3;
-static const u32 kAudioQueueBufferLength = 1 * 1024;
-
-static SDL_AudioSpec	mLINUXAudioData;
 SDL_AudioDeviceID audio_device;
-static Uint8 *audio_pos; // global pointer to the audio buffer to be played
-static Uint32 audio_len; // remaining length of the sample we have to play
 
+struct Sample {
+  s16 L;
+  s16 R;
+};
 
 class AudioPluginSDL : public CAudioPlugin
 {
@@ -59,30 +35,20 @@ public:
 	virtual EProcessResult	ProcessAList();
 
 	void					AddBuffer(void * ptr, u32 length);	// Uploads a new buffer and returns status
-
 	void					StopAudio();						// Stops the Audio PlayBack (as if paused)
 	void					StartAudio();						// Starts the Audio PlayBack (as if unpaused)
 
 	static void				AudioSyncFunction(void * arg);
-	static void 			AudioCallback(void *userdata, Uint8 *stream, int len);
-	static u32 				AudioThread(void * arg);
+	static int 				AudioThread(void * arg);
 
 private:
-	CAudioBuffer			mAudioBuffer;
-	u32								mFrequency;
-	ThreadHandle 			mAudioThread;
-	volatile bool			mKeepRunning;	// Should the audio thread keep running?
-	volatile u32 			mBufferLenMs;
+	u32						mFrequency;
+	SDL_Thread*  			mAudioThread;
 };
 
 AudioPluginSDL::AudioPluginSDL()
-:	mAudioBuffer( kAudioBufferSize )
-,	mFrequency( 44100 )
-,	mAudioThread( kInvalidThreadHandle )
-,	mKeepRunning( false )
-,	mBufferLenMs( 0 )
-{
-}
+:	mFrequency( 44100 )
+{}
 
 AudioPluginSDL::~AudioPluginSDL()
 {
@@ -106,7 +72,6 @@ void AudioPluginSDL::DacrateChanged(int system_type)
 	u32 dacrate   = Memory_AI_GetRegister(AI_DACRATE_REG);
 	u32	frequency = clock / (dacrate + 1);
 
-	DBGConsole_Msg(0, "Audio frequency: %d", frequency);
 	mFrequency = frequency;
 }
 
@@ -118,11 +83,6 @@ void AudioPluginSDL::LenChanged()
 		u32	length  = Memory_AI_GetRegister(AI_LEN_REG);
 
 		AddBuffer( g_pu8RamBase + address, length );
-	}
-	else
-	{
-		//This is disabled since it shutsdown SDL
-		//StopAudio();
 	}
 }
 
@@ -153,47 +113,31 @@ EProcessResult AudioPluginSDL::ProcessAList()
 
 void AudioPluginSDL::AddBuffer(void * ptr, u32 length)
 {
-	if (length == 0)
-		return;
+    if (length == 0)
+        return;
 
-	if (mAudioThread == kInvalidThreadHandle)
-		StartAudio();
+    if (mAudioThread == nullptr)
+        StartAudio();
 
-	u32 num_samples = length / sizeof( Sample );
+    u32 num_samples = length / sizeof(Sample);
 
-	mAudioBuffer.AddSamples( reinterpret_cast<const Sample *>(ptr), num_samples, mFrequency, kOutputFrequency );
-
-	u32 remaining_samples = mAudioBuffer.GetNumBufferedSamples();
-	mBufferLenMs = (1000 * remaining_samples) / kOutputFrequency;
-	float ms = (float)num_samples * 1000.f / (float)mFrequency;
-	DPF_AUDIO("Queuing %d samples @%dHz - %.2fms - bufferlen now %d\n",
-		num_samples, mFrequency, ms, mBufferLenMs);
+    // Queue the samples into SDL's audio buffer
+    if(SDL_QueueAudio(audio_device, ptr, num_samples * sizeof(Sample)) != 0) {
+        DBGConsole_Msg(0, "SDL_QueueAudio error: %s", SDL_GetError());
+        return;
+    }
 }
 
-void AudioPluginSDL::AudioCallback(void *userdata, Uint8 *stream, int len)
-{
-	AudioPluginSDL* plugin = static_cast<	AudioPluginSDL*>(userdata);
-
-	if(len <= 0){
-		return;
-	}
-
-	plugin->mAudioBuffer.Drain( reinterpret_cast< Sample * >( stream ), 4096);
-
-}
-
-u32 AudioPluginSDL::AudioThread(void * arg)
+int AudioPluginSDL::AudioThread(void * arg)
 {
 	AudioPluginSDL * plugin = static_cast<	AudioPluginSDL *>(arg);
 
-	// opening an audio device:
 	SDL_AudioSpec audio_spec;
-	SDL_zero(audio_spec);
-	audio_spec.freq = 44100;
+	audio_spec.freq = plugin->mFrequency;
 	audio_spec.format = AUDIO_S16SYS;
 	audio_spec.channels = 2;
 	audio_spec.samples = 4096;
-	audio_spec.callback = &AudioCallback;
+	audio_spec.callback = NULL;
 	audio_spec.userdata = plugin;
 
 	audio_device = SDL_OpenAudioDevice(
@@ -206,58 +150,32 @@ u32 AudioPluginSDL::AudioThread(void * arg)
 	return 0;
 }
 
-void AudioPluginSDL::AudioSyncFunction(void * arg)
-{
-	AudioPluginSDL * plugin = static_cast<AudioPluginSDL *>(arg);
-#if DEBUG_AUDIO
-	static u64 last_time = 0;
-	u64 now;
-	NTiming::GetPreciseTime(&now);
-	if (last_time == 0) last_time = now;
-	DPF_AUDIO("VBL: %dms elapsed. Audio buffer len %dms\n", (s32)NTiming::ToMilliseconds(now-last_time), plugin->mBufferLenMs);
-	last_time = now;
-#endif
-
-	u32 buffer_len = plugin->mBufferLenMs;	// NB: copy this volatile to a local var so that we have a consistent view for the remainder of this function.
-	if (buffer_len > kMaxBufferLengthMs)
-	{
-		ThreadSleepMs(buffer_len - kMaxBufferLengthMs);
-	}
-}
-
 void AudioPluginSDL::StartAudio()
 {
-	if (mAudioThread != kInvalidThreadHandle)
-		return;
-
-	mKeepRunning = true;
-
-	mAudioThread = CreateThread("Audio", &AudioThread, this);
-	if (mAudioThread == kInvalidThreadHandle)
+	mAudioThread = SDL_CreateThread(&AudioThread, "Audio", this);
+	
+	if (mAudioThread == nullptr)
 	{
 		DBGConsole_Msg(0, "Failed to start the audio thread!");
-		mKeepRunning = false;
-		FramerateLimiter_SetAuxillarySyncFunction(NULL, NULL);
+		FramerateLimiter_SetAuxillarySyncFunction(nullptr, nullptr);
 	}
-
 }
 
 void AudioPluginSDL::StopAudio()
 {
-	if (mAudioThread == kInvalidThreadHandle)
+	if (mAudioThread == nullptr)
 		return;
 
-	// Tell the thread to stop running.
-	mKeepRunning = false;
-
-	if (mAudioThread != kInvalidThreadHandle)
+	if (mAudioThread != nullptr)
 	{
-		JoinThread(mAudioThread, -1);
-		mAudioThread = kInvalidThreadHandle;
+		int threadReturnValue;
+		SDL_WaitThread(mAudioThread, &threadReturnValue);
+		mAudioThread = nullptr;
 	}
 
+    // Clear the remaining audio data in SDL's audio buffer
+    SDL_ClearQueuedAudio(audio_device);
 	SDL_CloseAudioDevice(audio_device);
-
 }
 
 std::unique_ptr<CAudioPlugin> CreateAudioPlugin()
