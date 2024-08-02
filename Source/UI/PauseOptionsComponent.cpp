2a5888ae7c1a8af2f44f01ed979e1b3339589393
wally4000 <catapultit@me.com>
2024-08-02 15:21:08 +1000

    Use correct definition for saveslot

--- Source/UI/PauseOptionsComponent.cpp
+++ Source/UI/PauseOptionsComponent.cpp
@@ -234,7 +234,7 @@ void	IPauseOptionsComponent::CheatOptions()
 
 void	IPauseOptionsComponent::SaveState()
 {
-auto onSaveStateSlotSelected = [this](const char* slot) { this->OnSaveStateSlotSelected(slot);
+auto onSaveStateSlotSelected = [this](const std::filesystem::path slot) { this->OnSaveStateSlotSelected(slot);
 };
 
 auto component = CSavestateSelectorComponent::Create(mpContext, CSavestateSelectorComponent::AT_SAVING, onSaveStateSlotSelected, g_ROM.settings.GameName.c_str());
@@ -247,7 +247,7 @@ auto component = CSavestateSelectorComponent::Create(mpContext, CSavestateSelect
 
 void	IPauseOptionsComponent::LoadState()
 {
-auto onLoadStateSlotSelected = [this](const char* slot) {
+auto onLoadStateSlotSelected = [this](const std::filesystem::path slot) {
     this->OnLoadStateSlotSelected(slot);
 };
 
