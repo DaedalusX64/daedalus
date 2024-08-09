2e9ed6cce26cbf838a24a4342e9a46e95a861b22
wally4000 <catapultit@me.com>
2024-08-09 19:16:08 +1000

    Remove Dynamo reference from PauseOptions

--- Source/UI/PauseOptionsComponent.cpp
+++ Source/UI/PauseOptionsComponent.cpp
@@ -22,7 +22,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
 #include "Core/CPU.h"
-#include "DynaRec/Dynamo.h"
 #include "Core/ROM.h"
 #include "Interface/SaveState.h"
 #include "HLEGraphics/DisplayListDebugger.h"
