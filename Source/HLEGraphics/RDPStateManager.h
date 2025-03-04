7f524d4045e76388b7c1baeed319d201e2da99a5
Wally4000 <catapultit@me.com>
2025-03-05 07:06:10 +1100

    Whoops. Revert this one

--- Source/HLEGraphics/RDPStateManager.h
+++ Source/HLEGraphics/RDPStateManager.h
@@ -95,9 +95,7 @@ private:
 
 extern CRDPStateManager		gRDPStateManager;
 
-#ifdef DAEDALUS_DEBUG_DISPLAYLIST
 extern RDP_OtherMode		gRDPOtherMode;
-#endif
 
 extern u32 gTlutLoadAddresses[ 4096 >> 6 ];
 
