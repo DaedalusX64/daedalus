9cebbb9cffe3d2d9a3aad69bda8f283a5db25f45
Wally4000 <catapultit@me.com>
2025-07-16 13:05:58 +1000

    Restore PSP and CTR Threading

--- Source/UI/GlobalSettingsComponent.cpp
+++ Source/UI/GlobalSettingsComponent.cpp
@@ -35,7 +35,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Menu.h"
 
 
-#include "System/Thread.h"
+#include "System/Thread/Thread.h"
 #include "Utility/FramerateLimiter.h"
 #include "Interface/Preferences.h"
 
