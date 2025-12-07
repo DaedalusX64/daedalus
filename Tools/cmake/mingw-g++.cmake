e969da65c16b040e27aeb3f1cc9f98f913fdbc16
Wally4000 <catapultit@me.com>
2025-12-07 14:49:52 +1100

    Fix syntax error

---
+++ Tools/cmake/mingw-g++.cmake
@@ -0,0 +1,27 @@
+# Tools/cmake/mingw-g++.cmake
+
+# Cross-compiling to Windows
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(TRIPLET x86_64-w64-mingw32)
+
+# Custom cross root (change if you want to use /usr/x86_64-w64-mingw32)
+set(CROSS_ROOT /usr/${TRIPLET})
+
+set(CMAKE_INSTALL_PREFIX "${CROSS_ROOT}")
+
+set(CMAKE_C_COMPILER   ${TRIPLET}-gcc-posix)
+set(CMAKE_CXX_COMPILER ${TRIPLET}-g++-posix)
+set(CMAKE_RC_COMPILER  ${TRIPLET}-windres)
+
+# Where to search for includes/libs for the target
+set(CMAKE_FIND_ROOT_PATH "${CROSS_ROOT}")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+
+
+
