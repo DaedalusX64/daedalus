39fbd996ac28d6fc1d98d13e3eba9dd90fdf633b
wally4000 <catapultit@me.com>
2026-08-11 13:47:47 +1000

    Add Ming-g++ cmake file

---
+++ Tools/cmake/mingw-g++.cmake
@@ -0,0 +1,29 @@
+# Tools/cmake/mingw-g++.cmake
+
+# Cross-compiling to Windows
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(TRIPLET x86_64-w64-mingw32)
+
+# Custom cross root (change if you want to use /usr/x86_64-w64-mingw32)
+set(CROSS_ROOT /usr/${TRIPLET})
+
+set(CMAKE_INSTALL_PREFIX "${CROSS_ROOT}")
+
+set(CMAKE_C_COMPILER   ${TRIPLET}-gcc)
+set(CMAKE_CXX_COMPILER ${TRIPLET}-g++)
+set(CMAKE_RC_COMPILER  ${TRIPLET}-windres)
+
+# Where to search for includes/libs for the target
+set(CMAKE_FIND_ROOT_PATH "${CROSS_ROOT}")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# Optional: static-ish linking
+set(CMAKE_EXE_LINKER_FLAGS
+    "${CMAKE_EXE_LINKER_FLAGS} -static-libstdc++ -static-libgcc")
